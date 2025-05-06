10264e9b08fee110b75e743831eddf629af8e837
jardeldyonisio <jardel.dyonisio@hotmail.com>
2025-05-06 14:12:24 -0300

    :sparkles: added navigation and description launch

---
+++ launch/navigation.launch.py
@@ -0,0 +1,25 @@
+import os
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from ament_index_python.packages import get_package_share_directory
+
+# This launch file runs nav2_stack and sensors, but not the robot description
+
+def generate_launch_description():
+
+    navigation = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory("fbot_navigation"), 'launch', 'navigation.launch.py')
+        
+        ),
+        launch_arguments={
+            'use_description': 'false'
+        }.items()
+    )
+
+    return LaunchDescription([
+        navigation,
+    ])
