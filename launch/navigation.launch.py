2f8615703137096e689b5e2854d6016bfd8da1e8
jardeldyonisio <jardel.dyonisio@hotmail.com>
2025-05-12 09:08:11 -0300

    :sparkles: added navigation launch

---
+++ launch/navigation.launch.py
@@ -0,0 +1,23 @@
+import os
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    navigation = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory("fbot_navigation"), 'launch', 'navigation.launch.py')
+
+        ),
+        launch_arguments={
+            'use_description': 'false'
+        }.items()
+    )
+
+    return LaunchDescription([
+        navigation,
+    ])
