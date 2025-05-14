32c25ad6ee8c9e795fcb007959d6172344225d58
Pedro Edom <pedroedomnunes@gmail.com>
2025-05-14 13:02:15 -0300

    :sparkles: feat: add interbotix arm launch

--- launch/interbotix_arm.launch.py
+++ launch/interbotix_arm.launch.py
@@ -2,6 +2,7 @@ import os
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from ament_index_python.packages import get_package_share_directory
@@ -10,11 +11,12 @@ def generate_launch_description():
 
     arm = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory("interbotix_xsarm_moveit"), 'launch', 'xsarm_moveit.launch.py')
+            os.path.join(get_package_share_directory("interbotix_xsarm_control"), 'launch', 'xsarm_control.launch.py')
         ),
         launch_arguments={
-            'robot_model': 'wx200',
-            'hardware_type': 'fake',
+            'robot_model': LaunchConfiguration('robot_model', default='wx200'),
+            'hardware_type': LaunchConfiguration('hardware_type', default='actual'),
+            'use_sim': LaunchConfiguration('use_sim', default='false'),
         }.items()
     )
 
