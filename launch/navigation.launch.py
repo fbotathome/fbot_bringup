91f2fbd11e20c97d51101b5f09c7d959c4c4a6c7
jardeldyonisio <jardel.dyonisio@hotmail.com>
2025-06-11 17:05:43 -0300

    :construction: fixing nav launch

--- launch/navigation.launch.py
+++ launch/navigation.launch.py
@@ -1,16 +1,16 @@
 import os
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration
 
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-    use_keepout_arg =
-        DeclareLaunchArgument(
+    use_keepout_arg = DeclareLaunchArgument(
             'use_keepout_zones',
             default_value='false',
             description='Parameter to include or not keepout zones on the navigation'
@@ -31,8 +31,7 @@ def generate_launch_description():
 
     navigation_keepout = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory("fbot_navigation"), 'launch', 'navigation.launch.py')
-
+            os.path.join(get_package_share_directory("fbot_navigation"), 'launch', 'navigation_keepout.launch.py')
         ),
         launch_arguments={
             'use_description': 'false',
@@ -42,6 +41,7 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        use_keepout_arg,
         navigation,
         navigation_keepout
     ])
