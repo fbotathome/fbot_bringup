3f43c58164777046fe6a9aad8d0f1c2753a7f5f1
vitoranello <vitoranello@gmail.com>
2025-06-11 15:50:53 -0300

    Remove comment

--- launch/synthesizer_speech.launch.py
+++ launch/synthesizer_speech.launch.py
@@ -1,14 +1,10 @@
 import os
-
 from launch import LaunchDescription
-from launch.conditions import IfCondition, UnlessCondition
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
 from ament_index_python.packages import get_package_share_directory
-
-#THIS LAUNCHFILE IS FOR TESTING PURPOSES ONLY, IT SHOULD NOT BE USED FOR NOW.
 
 def generate_launch_description():
 
