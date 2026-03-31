181cb80de87c06347def909102701ccca8e33958
Gui-MB <guiburkert@gmail.com>
2026-03-31 11:18:05 -0300

    adding parameters to launch

--- launch/vlm.launch.py
+++ launch/vlm.launch.py
@@ -1,12 +1,35 @@
 import os
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'vlm_api_type',
+            default_value='ollama',
+            description='Type of the VLM API. Must be one of: [openai, ollama, google-genai]'
+        ))
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'vlm_api_model',
+            default_value='qwen3:14b',
+            description='Model to use for the VLM API. Example: gemma3:4b, gemini-1.5-pro:8b, etc.'
+        ))
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'vlm_api_host',
+            default_value='http://192.168.0.102:11434',
+            description='Host URL for the VLM API. Must be set for openai and ollama.'
+        ))
 
     vlm = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
